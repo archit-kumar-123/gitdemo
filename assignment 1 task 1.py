60087a83c29fb204fd9679e24db66de96b0d0edd
unknown <kumararchit74@gmail.com>
2025-09-28 20:26:16 +0530

    anu

---
+++ assignment 1 task 1.py
@@ -0,0 +1,17 @@
+# Task 1 : Perform basic mathematical operations
+a=input("Enter the first number : ")
+b=input("Enter the second number : ")
+a=int(a)
+b=int(b)
+Addition=a+b
+
+Subtraction=a-b
+
+Multiplication=a*b
+
+Division=a/b
+
+print("Addition",Addition)
+print("Subtraction",Subtraction)
+print("Multiplication",Multiplication)
+print("Division",Division)
